432f8fba96c0528899a3b678296230a8ccca6ee1
bodii <1401097251@qq.com>
2022-12-14 21:42:26 +0800

    add: apply table sql

---
+++ go-api-service/ochat/databases/apply.sql
@@ -0,0 +1,14 @@
+drop table if exists apply;
+create table apply (
+    id bigint not null auto_increment primary key comment '申请好友表id',
+	petitioner bigint not null default 0 comment '申请者userid',
+	responder bigint not null default 0 comment '应答者userid(如果是群，则是管理者userid)',
+	type tinyint not null default 1 comment '申请类型,1:好友;2:群',
+	friend_id bigint not null default 0 comment '被申请好友的用户id(如果是群，则是群id)',
+	comment varchar(250) not null default '' comment '申请时的留言',
+	status tinyint not null default 0 comment '应答者是否同意,-1:拒绝;0:未查看;1:已查看;2:同意',
+	created_at datetime(6) not null comment '创建时间',
+	updated_at datetime(6) not null comment '更新时间',
+	key apply_responder_status_type(responder, status, type),
+    key community_petitioner(petitioner)
+) engine=innodb default charset=utf8mb4 comment '申请好友(加群)表';
