03a71e0f29c37d5bf0b81553a53cc59bf785d7a7
bodii <1401097251@qq.com>
2022-12-15 18:54:19 +0800

    modify: model file adjustment for compatibility with xorm library syntax

--- go-api-service/ochat/models/message.go
+++ go-api-service/ochat/models/message.go
@@ -46,9 +46,9 @@ const (
 // 消息体核心model
 type Message struct {
 	Id                int64  `xorm:"pk autoincr bigint not null comment('消息id')" json:"id,omitempty" form:"id"`
-	Mode              int    `xorm:"tinyint not null default 1 comment('模式,1:单聊;2:群聊')" json:"mode" form:"mode"`
-	SenderId          int64  `xorm:"bigint not null default 0 comment('发送用户id')" json:"sender_id" form:"sender_id"`
-	ReceiverId        int64  `xorm:"bigint not null default 0 comment('接收方id, [mode=1]:对方id,[mode=2]:群id')" json:"receiver_id,omitempty" form:"receiver_id"`
+	SenderId          int64  `xorm:"bigint index('message_sender_id') not null default 0 comment('发送用户id')" json:"sender_id" form:"sender_id"`
+	ReceiverId        int64  `xorm:"bigint index('message_receiver_id_mode') not null default 0 comment('接收方id, [mode=1]:对方id,[mode=2]:群id')" json:"receiver_id,omitempty" form:"receiver_id"`
+	Mode              int    `xorm:"tinyint index('message_receiver_id_mode') not null default 1 comment('模式,1:单聊;2:群聊')" json:"mode" form:"mode"`
 	Type              int    `xorm:"tinyint not null default 1 comment('消息内容类型,1:文字;2:图片;3:表情;4:录音;5:名片;6:红包;7:音频文件;8:视频文件;9:接龙;')" json:"type" form:"type"`
 	Content           string `xorm:"mediumtext not null comment('消息内容')" json:"content,omitempty" form:"content"`
 	Pic               string `xorm:"varchar(220) not null comment('预览图片')" json:"pic,omitempty" form:"pic"`
--- go-api-service/ochat/models/user.go
+++ go-api-service/ochat/models/user.go
@@ -21,8 +21,8 @@ const (
 // 接入鉴权/用户信息表
 type User struct {
 	Id         int64     `xorm:"pk autoincr bigint not null comment('用户id,user_id')" form:"id" json:"id"`
-	Mobile     string    `xorm:"varchar(20) unique 'user_mobile' not null default '' comment('手机号')" form:"mobile" json:"mobile"`
-	Username   string    `xorm:"varchar(20) unique 'user_username' not null default '' comment('用户名')" form:"username" json:"username"`
+	Mobile     string    `xorm:"varchar(20) unique not null default '' comment('手机号')" form:"mobile" json:"mobile"`
+	Username   string    `xorm:"varchar(20) unique not null default '' comment('用户名')" form:"username" json:"username"`
 	Nickname   string    `xorm:"varchar(20) not null default '' comment('用户id')" form:"nickname" json:"nickname"`
 	Password   string    `xorm:"varchar(40) not null default '' comment('密码')" form:"password" json:"password"`
 	About      string    `xorm:"varchar(250) not null default '' comment('简单描述')" form:"about" json:"about"`
@@ -31,7 +31,7 @@ type User struct {
 	Avatar     string    `xorm:"varchar(160) not null default '' comment('头像')" form:"avatar" json:"avatar"`
 	Sex        int       `xorm:"tinyint not null default 1 comment('性别,0:无;1:男;2:女')" form:"sex" json:"sex"`
 	Online     int64     `xorm:"bigint not null default 0 comment('在线时长')" form:"online" json:"online"`
-	Status     string    `xorm:"tinyint index 'user_status' not null default 1 comment('状态是否可用, 1:可用;0:冻结')" form:"" json:""`
+	Status     string    `xorm:"tinyint index('user_status') not null default 1 comment('状态是否可用, 1:可用;0:冻结')" form:"" json:""`
 	Created_at time.Time `xorm:"datetime(6) not null comment('创建时间')" form:"created_at" json:"created_at"`
 	Updated_at time.Time `xorm:"datetime(6) not null comment('更新时间')" form:"updated_at" json:"updated_at"`
 }
